39e9e61a1c74652bc9169a252b5d095e4d791855
KYUNGJINS <adele0116@naver.com>
2023-04-14 19:17:43 +0900

    04-14

---
+++ franchise-project-settings(backup)/src/main/java/com/ktds/fr/rv/vo/RvVO.java
@@ -0,0 +1,92 @@
+package com.ktds.fr.rv.vo;		
+	
+/**
+ * 리뷰(RV)
+ * @author User
+ *
+ */
+public class RvVO {		
+		
+	// 리뷰ID
+	private String rvId; 
+	// 회원 ID
+	private String mbrId;
+	// 주문 ID
+	private String odrId;
+	// 제목
+	private String rvTtl;	
+	// 내용
+	private String rvCntnt;	
+	// 좋아요/싫어요
+	private String rvLkDslk;	
+	// 등록일
+	private String rvRgstDt;	
+	// 수정일
+	private String mdfyDt;	
+	// 사용유무
+	private String useYn;
+	// 삭제여부
+	private String delYn;	
+	
+	public String getRvId() {	
+		return rvId;
+	}	
+	public void setRvId(String rvId) {	
+		this.rvId = rvId;
+	}	
+	public String getMbrId() {	
+		return mbrId;
+	}	
+	public void setMbrId(String mbrId) {	
+		this.mbrId = mbrId;
+	}	
+	public String getOdrId() {	
+		return odrId;
+	}	
+	public void setOdrId(String odrId) {	
+		this.odrId = odrId;
+	}	
+	public String getRvTtl() {	
+		return rvTtl;
+	}	
+	public void setRvTtl(String rvTtl) {	
+		this.rvTtl = rvTtl;
+	}	
+	public String getRvCntnt() {	
+		return rvCntnt;
+	}	
+	public void setRvCntnt(String rvCntnt) {	
+		this.rvCntnt = rvCntnt;
+	}	
+	public String getRvLkDslk() {	
+		return rvLkDslk;
+	}	
+	public void setRvLkDslk(String rvLkDslk) {	
+		this.rvLkDslk = rvLkDslk;
+	}	
+	public String getRvRgstDt() {	
+		return rvRgstDt;
+	}	
+	public void setRvRgstDt(String rvRgstDt) {	
+		this.rvRgstDt = rvRgstDt;
+	}	
+	public String getMdfyDt() {	
+		return mdfyDt;
+	}	
+	public void setMdfyDt(String mdfyDt) {	
+		this.mdfyDt = mdfyDt;
+	}	
+	public String getUseYn() {	
+		return useYn;
+	}	
+	public void setUseYn(String useYn) {	
+		this.useYn = useYn;
+	}	
+	public String getDelYn() {	
+		return delYn;
+	}	
+	public void setDelYn(String delYn) {	
+		this.delYn = delYn;
+	}	
+		
+}		
